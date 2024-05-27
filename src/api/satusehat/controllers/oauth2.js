f767a21a197a7a2c83a0750d0d01c3b2306e5c78
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-27 11:54:44 +0700

    save

--- src/api/satusehat/controllers/oauth2.js
+++ src/api/satusehat/controllers/oauth2.js
@@ -57,7 +57,9 @@ class OAuth2 extends Controller {
             //     "refresh_count": "0",
             //     "status": "approved"
             //   }
-            res.locals.storage.token=result.access_token
+            if(process.env.client_id){
+                res.locals.storage.token=result.access_token
+            }
 
             res.json(result);
         } catch (error) {
@@ -89,48 +91,51 @@ class OAuth2 extends Controller {
             //       }
             //     ]
             //   }
-            
-            const {params,query,body} = req
-            const result = await res.locals.service.get({params,query,body});
-            // console.log(JSON.stringify(result))
-            if(result?.issue?.[0]?.code=='invalid-access-token'&&process.env.client_id){
-                // refresh
-                const result = await res.locals.service.post({
-                    params: {
-                    },
-                    query: {
-                       "grant_type": query["grant_type"]??"client_credentials",
-                    },
-                    body: {
-                        "client_id": process.env.client_id,// "{{client_id}}",
-                        "client_secret": process.env.client_secret,// "{{client_secret}}"
-                    },
-    // 
-    // 
-                    // body,
-                });
+
+            if(process.env.client_id){
+                const {params,query,body} = req
+                const result = await res.locals.service.get({params,query,body});
                 // console.log(JSON.stringify(result))
-    
-                // {
-                //     "refresh_token_expires_in": "0",
-                //     "api_product_list": "[api-satusehat-prod]",
-                //     "api_product_list_json": [
-                //       "api-satusehat-prod"
-                //     ],
-                //     "organization_name": "ihs-prod-1",
-                //     "developer.email": "rahmanrahimrs@gmail.com",
-                //     "token_type": "BearerToken",
-                //     "issued_at": "1716046940319",
-                //     "client_id": "bfeuWZr54cjGPXUUkG2GHUoVy1hVCPpiXmK8kV3VhFjRIo9D",
-                //     "access_token": "LVqhSG1jnmudBNhHb58GQj1KPzDL",
-                //     "application_name": "94805e1b-2d54-4af6-b0fe-c2143517b55a",
-                //     "scope": "",
-                //     "expires_in": "14399",
-                //     "refresh_count": "0",
-                //     "status": "approved"
-                //   }
-                res.locals.storage.token=result.access_token
+                if(result?.issue?.[0]?.code=='invalid-access-token'){
+                    // refresh
+                    const result = await res.locals.service.post({
+                        params: {
+                        },
+                        query: {
+                           "grant_type": query["grant_type"]??"client_credentials",
+                        },
+                        body: {
+                            "client_id": process.env.client_id,// "{{client_id}}",
+                            "client_secret": process.env.client_secret,// "{{client_secret}}"
+                        },
+        // 
+        // 
+                        // body,
+                    });
+                    // console.log(JSON.stringify(result))
+        
+                    // {
+                    //     "refresh_token_expires_in": "0",
+                    //     "api_product_list": "[api-satusehat-prod]",
+                    //     "api_product_list_json": [
+                    //       "api-satusehat-prod"
+                    //     ],
+                    //     "organization_name": "ihs-prod-1",
+                    //     "developer.email": "rahmanrahimrs@gmail.com",
+                    //     "token_type": "BearerToken",
+                    //     "issued_at": "1716046940319",
+                    //     "client_id": "bfeuWZr54cjGPXUUkG2GHUoVy1hVCPpiXmK8kV3VhFjRIo9D",
+                    //     "access_token": "LVqhSG1jnmudBNhHb58GQj1KPzDL",
+                    //     "application_name": "94805e1b-2d54-4af6-b0fe-c2143517b55a",
+                    //     "scope": "",
+                    //     "expires_in": "14399",
+                    //     "refresh_count": "0",
+                    //     "status": "approved"
+                    //   }
+                    res.locals.storage.token=result.access_token
+                }
             }
+            
 
             next()
         } catch (error) {
